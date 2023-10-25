9ecdfc646b6582e2f4c2b7f1730d06f72d008f42
ralphbrynard <ralph.brynard@sophos.com>
2023-10-25 09:29:02 -0500

    adding windows build. Included temporary env var to run build and set to false

---
+++ scripts/windows/windows-prepare.ps1
@@ -0,0 +1,98 @@
+<#
+    .DESCRIPTION
+    Prepares a Windows guest operating system.
+#>
+
+param(
+    [string] $BUILD_USERNAME = $env:BUILD_USERNAME
+)
+
+$ErrorActionPreference = "Stop"
+
+# Optional: Import the Root CA certificate to the Trusted Root Certification Authorities.
+# This option will require the use of a file provisioner to copy the certificate to the guest.
+# Write-Output "Importing the Root CA certificate to the Trusted Root Certification Authorities..."
+# Import-Certificate -FilePath C:\windows\temp\root-ca.cer -CertStoreLocation 'Cert:\LocalMachine\Root' | Out-Null
+# Remove-Item C:\windows\temp\root-ca.cer -Confirm:$false
+
+# Optional: Import the Issuing CA certificate to the Trusted Root Certification Authoriries.
+# This option will require the use of a file provisioner to copy the certificate to the guest.
+# Write-Output "Importing the Issuing CA certificate to the Trusted Root Certification Authoriries..."
+# Import-Certificate -FilePath C:\windows\temp\issuing-ca.cer -CertStoreLocation 'Cert:\LocalMachine\CA' | Out-Null
+# Remove-Item C:\windows\temp\issuing-ca.cer -Confirm:$false
+
+# Set the Windows Explorer options.
+Write-Output "Setting the Windows Explorer options..."
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Value 1 | Out-Null
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0 | Out-Null
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideDrivesWithNoMedia" -Value 0 | Out-Null
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowSyncProviderNotifications" -Value 0 | Out-Null
+
+# Disable system hibernation.
+Write-Output "Disabling system hibernation..."
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Power\" -Name "HiberFileSizePercent" -Value 0 | Out-Null
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Power\" -Name "HibernateEnabled" -Value 0 | Out-Null
+
+# Disable TLS 1.0.s
+Write-Output "Disabling TLS 1.0..."
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.0" | Out-Null
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0" -Name "Server" | Out-Null
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0" -Name "Client" | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Name "Enabled" -Value 0 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Name "DisabledByDefault" -Value 1 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Name "Enabled" -Value 0 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Name "DisabledByDefault" -Value 1 | Out-Null
+
+# Disable TLS 1.1.
+Write-Output "Disabling TLS 1.1..."
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.1" | Out-Null
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Server" | Out-Null
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1" -Name "Client" | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client" -Name "Enabled" -Value 0 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client" -Name "DisabledByDefault" -Value 1 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server" -Name "Enabled" -Value 0 | Out-Null
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server" -Name "DisabledByDefault" -Value 1 | Out-Null
+
+# Installing Cloudbase-Init
+Write-Host " - Installing Cloudbase-Init ..."
+$msiLocation = 'https://cloudbase.it/downloads'
+$msiFileName = 'CloudbaseInitSetup_Stable_x64.msi'
+Invoke-WebRequest -Uri ($msiLocation + '/' + $msiFileName) -OutFile C:\$msiFileName
+Unblock-File -Path C:\$msiFileName
+Start-Process msiexec.exe -ArgumentList "/i C:\$msiFileName /qn /norestart RUN_SERVICE_AS_LOCAL_SYSTEM=1" -Wait
+Write-Host " - Configuring Cloudbase-Init for Proxmox ..."
+$confFile = 'cloudbase-init.conf'
+$confPath = "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\"
+$confContent = @"
+[DEFAULT]
+bsdtar_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\bsdtar.exe
+mtools_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\bin\
+verbose=true
+debug=true
+logdir=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\log\
+logfile=cloudbase-init.log
+default_log_levels=comtypes=INFO,suds=INFO,iso8601=WARN,requests=WARN
+local_scripts_path=C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts\
+metadata_services=cloudbaseinit.metadata.services.nocloudservice.NoCloudConfigDriveService
+plugins=cloudbaseinit.plugins.common.userdata.UserDataPlugin
+"@
+New-Item -Path $confPath -Name $confFile -ItemType File -Force -Value $confContent | Out-Null
+Start-Process sc.exe -ArgumentList "config cloudbase-init start= delayed-auto" -wait | Out-Null
+Remove-Item -Path ($confPath + "cloudbase-init-unattend.conf") -Confirm:$false 
+Remove-Item -Path ($confPath + "Unattend.xml") -Confirm:$false 
+Remove-Item C:\$msiFileName -Confirm:$false
+
+# Disable Password Expiration for the Administrator Accounts - (Administrator and Build)
+Write-Output "Disabling password expiration for the local Administrator accounts..."
+$localAdministrator = (Get-LocalUser | Where-Object { $_.sid -like '*-500' }).name
+Set-LocalUser -Name $localAdministrator -PasswordNeverExpires $true
+Set-LocalUser -Name $BUILD_USERNAME -PasswordNeverExpires $true
+
+# Enable Remote Desktop.
+Write-Output "Enabling Remote Desktop..."
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0 | Out-Null
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 0
+Enable-NetFirewallRule -Group '@FirewallAPI.dll,-28752'
+
+# End configuration
+Write-Host " - Configuration complete ..."
